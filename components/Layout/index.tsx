cf722bb77ed305ec00e9134ef1dcbb38b4938726
faryadicka <faryadicka98@gmail.com>
2022-10-31 17:10:52 +0700

    comment form

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -1,19 +1,19 @@
 import React from 'react';
 import { Box } from '@mui/material';
-import { useForm } from 'react-hook-form';
-import InputWithLabel from 'components/Input/InputWithLabel';
+// import { useForm } from 'react-hook-form';
+// import InputWithLabel from 'components/Input/InputWithLabel';
 
 const Layout = () => {
-    const form = useForm({
-        mode: 'all',
-        defaultValues: {
-            email: ''
-        }
-    });
+    // const form = useForm({
+    //     mode: 'all',
+    //     defaultValues: {
+    //         email: ''
+    //     }
+    // });
     return (
         <Box>
             <img src='/images/logo.png' alt='Prize Play' width='99.89px' height='46.56px' />
-            <InputWithLabel form={form} name='email' type='email' rules={{ required: true }} />
+            {/* <InputWithLabel form={form} name='email' type='email' rules={{ required: true }} /> */}
         </Box>
     );
 };
