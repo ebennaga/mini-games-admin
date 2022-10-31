5092c7e4d0c9e6f1f0bb9429e35762d13bc0e35a
faryadicka <faryadicka98@gmail.com>
2022-10-31 17:47:29 +0700

    fix Input search

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -1,19 +1,22 @@
 import React from 'react';
 import { Box } from '@mui/material';
-// import { useForm } from 'react-hook-form';
+import InputSearch from 'components/Input/InputSearch';
+import { useForm } from 'react-hook-form';
 // import InputWithLabel from 'components/Input/InputWithLabel';
 
 const Layout = () => {
-    // const form = useForm({
-    //     mode: 'all',
-    //     defaultValues: {
-    //         email: ''
-    //     }
-    // });
+    const form = useForm({
+        mode: 'all',
+        defaultValues: {
+            search: ''
+        }
+    });
     return (
         <Box>
             <img src='/images/logo.png' alt='Prize Play' width='99.89px' height='46.56px' />
-            {/* <InputWithLabel form={form} name='email' type='email' rules={{ required: true }} /> */}
+            <Box sx={{ mt: '10px' }}>
+                <InputSearch form={form} label='Search' name='search' />
+            </Box>
         </Box>
     );
 };
