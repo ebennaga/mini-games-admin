aef8df113776048f0652e3fa6ed1bf901230e64e
rintosaputro <saputrorinto@gmail.com>
2022-11-04 16:39:26 +0700

    add component input select

---
+++ components/Input/InputSelect.tsx
@@ -0,0 +1,72 @@
+import React from 'react';
+import { FormControl, Select, MenuItem, Typography, OutlinedInput } from '@mui/material';
+import { Controller } from 'react-hook-form';
+
+interface InputSelectProps {
+    form: any;
+    name: string;
+    dataSelect: any;
+    title: string;
+    placeholder: string;
+}
+
+const InputSelect: React.FC<InputSelectProps> = ({ form, name, dataSelect, title, placeholder }) => {
+    const handleChange = (e: any) => {
+        form.setValue(name, e.target.value);
+    };
+
+    return (
+        <FormControl fullWidth>
+            <Typography
+                component='span'
+                fontSize='12px'
+                sx={{ background: '#fff', position: 'absolute', top: '-9px', zIndex: 1, left: 14, px: 1 }}
+            >
+                {title}
+            </Typography>
+            <Controller
+                name={name}
+                control={form.control}
+                render={() => {
+                    return (
+                        <Select
+                            fullWidth
+                            displayEmpty
+                            value={form.watch(name)}
+                            onChange={handleChange}
+                            input={<OutlinedInput />}
+                            placeholder='Select Category'
+                            labelId='demo-simple-select-label'
+                            id='demo-simple-select'
+                            label='Role Code'
+                            sx={{
+                                '&.Mui-focused .MuiOutlinedInput-notchedOutline': {
+                                    borderColor: '#A54CE5'
+                                },
+                                '&:hover .MuiOutlinedInput-notchedOutline': {
+                                    borderColor: '#A54CE5'
+                                },
+                                '& .css-11u53oe-MuiSelect-select-MuiInputBase-input-MuiOutlinedInput-input': {
+                                    color: form.watch(name) ? 'rgba(0, 0, 0,1)' : 'rgba(0, 0, 0, 0.38)'
+                                }
+                            }}
+                        >
+                            <MenuItem sx={{ fontSize: '16px' }} disabled value=''>
+                                {placeholder}
+                            </MenuItem>
+                            {dataSelect.map((item: any) => {
+                                return (
+                                    <MenuItem value={item.id} key={item.id}>
+                                        {item.title}
+                                    </MenuItem>
+                                );
+                            })}
+                        </Select>
+                    );
+                }}
+            />
+        </FormControl>
+    );
+};
+
+export default InputSelect;
