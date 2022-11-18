e7fc8f5fe5ab6f8d99e213af76c984f0a2d85c29
rintosaputro <saputrorinto@gmail.com>
2022-11-18 14:09:50 +0700

    add fetch data exchange

---
+++ containers/ExchangeRates/TableExchange.tsx
@@ -0,0 +1,162 @@
+import {
+    Checkbox,
+    FormControl,
+    FormControlLabel,
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableHead,
+    TableRow,
+    Typography
+} from '@mui/material';
+import React from 'react';
+
+interface TableExchangeProps {
+    name: string;
+    form: any;
+    nameIdxAppears: string;
+}
+
+const TableExchange: React.FC<TableExchangeProps> = ({ name, form, nameIdxAppears }) => {
+    const [dataTable, setDataTable] = React.useState<Array<any>>([...form.watch(name)]);
+    const [isAllChecked, setIsAllChecked] = React.useState<boolean>(false);
+
+    const handleCheckAll = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const isChecked = e.target.checked;
+        let resArr: any = [];
+        if (isChecked) {
+            dataTable.forEach((item: any) => {
+                const resObj = { ...item, isAction: true };
+                resArr = [...resArr, resObj];
+            });
+            setIsAllChecked(true);
+        } else {
+            dataTable.forEach((item: any) => {
+                const resObj = { ...item, isAction: false };
+                resArr = [...resArr, resObj];
+            });
+            setIsAllChecked(false);
+        }
+        setDataTable([...resArr]);
+        form.setValue(name, [...resArr]);
+    };
+
+    const handleCheck = async (e: React.ChangeEvent<HTMLInputElement>, data: any) => {
+        const isChecked = e.target.checked;
+        let newArr: any = [...dataTable];
+        dataTable.forEach((item: any, index: number) => {
+            if (item.id === data.id) {
+                const filter = newArr.filter((itm: any) => itm.id !== data.id);
+                if (isChecked) {
+                    newArr = [...filter, { ...data, isAction: true }];
+                } else {
+                    newArr = [...filter, { ...data, isAction: false }];
+                    setIsAllChecked(false);
+                }
+            }
+
+            if (index === dataTable.length - 1) {
+                newArr.sort((a: any, b: any) => a.id - b.id);
+                setDataTable([...newArr]);
+                form.setValue(name, [...newArr]);
+            }
+        });
+    };
+
+    return (
+        <TableContainer>
+            <Table aria-label='table participant'>
+                <TableHead>
+                    <TableRow sx={{ borderTop: '1px solid rgba(0,0,0,0.2)', bgcolor: '#F0F0F0' }}>
+                        <TableCell
+                            align='center'
+                            sx={{
+                                fontWeight: 400,
+                                fontSize: '16px',
+                                borderRight: '1px solid rgba(0,0,0,0.2)',
+                                borderLeft: '1px solid rgba(0,0,0,0.2)',
+                                width: '5%'
+                            }}
+                        >
+                            No.
+                        </TableCell>
+                        <TableCell sx={{ fontWeight: 400, fontSize: '16px', borderRight: '1px solid rgba(0,0,0,0.2)', width: '25%' }}>
+                            Coins
+                        </TableCell>
+                        <TableCell sx={{ fontWeight: 400, fontSize: '16px', borderRight: '1px solid rgba(0,0,0,0.2)', width: '15%' }}>
+                            IDR
+                        </TableCell>
+                        <TableCell sx={{ fontWeight: 400, fontSize: '16px', borderRight: '1px solid rgba(0,0,0,0.2)', width: '20%' }}>
+                            Effective Date
+                        </TableCell>
+                        <TableCell
+                            align='center'
+                            sx={{ fontWeight: 400, fontSize: '16px', borderRight: '1px solid rgba(0,0,0,0.2)', width: '10%' }}
+                        >
+                            <FormControl sx={{ '& .css-12wnr2w-MuiButtonBase-root-MuiCheckbox-root.Mui-checked': { color: '#A54CE5' } }}>
+                                <FormControlLabel
+                                    control={<Checkbox checked={!!isAllChecked} onChange={(e) => handleCheckAll(e)} />}
+                                    label='Action'
+                                />
+                            </FormControl>
+                        </TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {dataTable.map((item: any, index: number) => {
+                        const idxAppears = form.watch(nameIdxAppears);
+                        return (
+                            index >= idxAppears.startIndex &&
+                            index <= idxAppears.endIndex && (
+                                <TableRow key={item.id}>
+                                    <TableCell
+                                        align='center'
+                                        sx={{
+                                            fontWeight: 400,
+                                            fontSize: '16px',
+                                            borderRight: '1px solid rgba(0,0,0,0.2)',
+                                            borderLeft: '1px solid rgba(0,0,0,0.2)',
+                                            width: '5%'
+                                        }}
+                                    >
+                                        <Typography component='span'>{index + 1}.</Typography>
+                                    </TableCell>
+                                    <TableCell
+                                        sx={{ fontWeight: 400, fontSize: '16px', borderRight: '1px solid rgba(0,0,0,0.2)', width: '25%' }}
+                                    >
+                                        <Typography component='span'>{item?.coin}</Typography>
+                                    </TableCell>
+                                    <TableCell
+                                        sx={{ fontWeight: 400, fontSize: '16px', borderRight: '1px solid rgba(0,0,0,0.2)', width: '15%' }}
+                                    >
+                                        <Typography component='span'>{item?.price}</Typography>
+                                    </TableCell>
+                                    <TableCell
+                                        sx={{ fontWeight: 400, fontSize: '16px', borderRight: '1px solid rgba(0,0,0,0.2)', width: '20%' }}
+                                    >
+                                        <Typography component='span'>{item?.effective_until_at}</Typography>
+                                    </TableCell>
+                                    <TableCell
+                                        align='center'
+                                        sx={{ fontWeight: 400, fontSize: '16px', borderRight: '1px solid rgba(0,0,0,0.2)', width: '10%' }}
+                                    >
+                                        <FormControl
+                                            sx={{
+                                                '& .css-12wnr2w-MuiButtonBase-root-MuiCheckbox-root.Mui-checked': { color: '#A54CE5' }
+                                            }}
+                                        >
+                                            <Checkbox checked={!!item.isAction} onChange={(e) => handleCheck(e, item)} />
+                                        </FormControl>
+                                    </TableCell>
+                                </TableRow>
+                            )
+                        );
+                    })}
+                </TableBody>
+            </Table>
+        </TableContainer>
+    );
+};
+
+export default TableExchange;
--- containers/ExchangeRates/index.tsx
+++ containers/ExchangeRates/index.tsx
@@ -2,20 +2,93 @@ import React from 'react';
 import { Container, Box, Typography, ButtonBase, Paper } from '@mui/material';
 import Search from 'components/Search';
 import { useForm } from 'react-hook-form';
-import TableExchange from 'components/Table/TableExchange';
+// import TableExchange from 'components/Table/TableExchange';
 import { useRouter } from 'next/router';
+import PaginationCard from 'components/PaginationCard';
+import useAPICaller from 'hooks/useAPICaller';
+import useNotify from 'hooks/useNotify';
+import TableExchange from './TableExchange';
+import LoadingExchangeRates from './LoadingExchangeRates';
 
 const ExchangeRates = () => {
+    const [isLoading, setIsloading] = React.useState<boolean>(true);
+    const [data, setData] = React.useState<Array<any>>([]);
+
     const router = useRouter();
+    const { fetchAPI } = useAPICaller();
+    const notify = useNotify();
+
     const handleSearch = async () => {
         console.log('done');
     };
+
     const form = useForm({
         mode: 'all',
         defaultValues: {
-            search: ''
+            search: '',
+            idxAppears: { startIndex: 0, endIndex: 5 },
+            dataTable: data,
+            page: 1,
+            row: 5
         }
     });
+
+    // Event Next page
+    const handleNext = () => {
+        const input: any = form.watch();
+        const totalPage = Math.ceil(data.length / input.row);
+        if (input.page < totalPage) {
+            form.setValue('page', input.page + 1);
+        }
+    };
+
+    // Event Prev Page
+    const handlePrev = () => {
+        const input = form.watch();
+        if (input.page > 1) {
+            form.setValue('page', input.page - 1);
+        }
+    };
+
+    // Update useForm idxAppears value, while doing pagination events
+    React.useEffect(() => {
+        const page = form.watch('page');
+        const row = form.watch('row');
+
+        const startIndex = page * row - row;
+        const endIndex = startIndex + row - 1;
+
+        const result = { startIndex, endIndex };
+        form.setValue('idxAppears', result);
+    }, [form.watch('row'), form.watch('page')]);
+
+    // Get Data
+    const getData = async () => {
+        setIsloading(true);
+        try {
+            const response = await fetchAPI({
+                method: 'GET',
+                endpoint: 'exchange-rates'
+            });
+
+            if (response.status === 200) {
+                const resData = response.data.data.sort((a: any, b: any) => a.id - b.id);
+                setData(resData);
+                form.setValue('dataTable', resData);
+            }
+        } catch (err: any) {
+            notify(err.message, 'error');
+        }
+        setIsloading(false);
+    };
+
+    React.useEffect(() => {
+        getData();
+    }, []);
+
+    if (isLoading) {
+        return <LoadingExchangeRates />;
+    }
     return (
         <Container sx={{ mt: 12 }}>
             <Box sx={{ ml: -25, height: 120 }} component={Paper}>
@@ -43,9 +116,18 @@ const ExchangeRates = () => {
                     </Box>
                 </Box>
             </Box>
-            <Box sx={{ ml: -25, mt: 3 }} component={Paper}>
-                <TableExchange />
+            <Box sx={{ ml: -25, mt: 3 }}>
+                {/* <TableExchange /> */}
+                <TableExchange form={form} name='dataTable' nameIdxAppears='idxAppears' />
             </Box>
+            <PaginationCard
+                totalItem={data.length}
+                handleNext={handleNext}
+                handlePrev={handlePrev}
+                form={form}
+                nameRow='row'
+                namePage='page'
+            />
         </Container>
     );
 };
