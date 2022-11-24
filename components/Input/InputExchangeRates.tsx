ade9f38688a3d4a7ae414d8651cce8b515c86629
rintosaputro <saputrorinto@gmail.com>
2022-11-24 11:59:14 +0700

    feat integration add exchange

--- components/Input/InputExchangeRates.tsx
+++ components/Input/InputExchangeRates.tsx
@@ -1,71 +1,150 @@
+/* eslint-disable no-unused-vars */
 import React from 'react';
-import { Box, TextField, Stack, ButtonBase, Typography } from '@mui/material';
+import { Box, TextField, Stack, ButtonBase, Typography, CircularProgress } from '@mui/material';
 import Image from 'next/image';
+import FieldRequired from 'components/FieldRequired';
+import InputDate from './InputDate';
+import Input from './Input';
 
-const InputExchangeRates = () => {
+interface InputExchangeRatesProps {
+    effectiveName: string;
+    coinsName: string;
+    idrName: string;
+    form: any;
+    handleSubmit: any;
+    loadingSubmit: boolean;
+}
+
+const InputExchangeRates: React.FC<InputExchangeRatesProps> = ({
+    effectiveName,
+    coinsName,
+    idrName,
+    form,
+    handleSubmit,
+    loadingSubmit
+}) => {
     const values = {
         someDate: '2017-05-24'
     };
+
     return (
-        // <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
-        //     <Box>adad</Box>
-        //     <TextField
-        //         name='someDate'
-        //         label='Effective Date'
-        //         InputLabelProps={{ shrink: true, required: true }}
-        //         type='date'
-        //         defaultValue={values.someDate}
-        //     />
-        // </Box>
-        <Box sx={{ width: '100%' }}>
-            <Stack direction='row' spacing={30}>
-                <Box>Effective</Box>
-                <TextField
-                    sx={{ width: '30%' }}
-                    name='someDate'
-                    label='Effective Date'
-                    InputLabelProps={{ shrink: true, required: true }}
-                    type='date'
-                    defaultValue={values.someDate}
-                />
-            </Stack>
-            <Stack direction='row' spacing={32} sx={{ mt: 4 }}>
-                <Box>Coins</Box>
-                <TextField
-                    sx={{ width: '30%' }}
-                    name='Enter Amount'
-                    label='Enter Amount'
-                    InputProps={{
-                        // eslint-disable-next-line jsx-a11y/alt-text
-                        // startAdornment: <img src='/icons/navbar/account.png' />
-                        startAdornment: <Image src='/images/coin.png' alt='picture' width={20} height={20} />
-                    }}
-                />
-            </Stack>
-            <Stack direction='row' spacing={22} sx={{ mt: 4 }}>
-                <Box>Indonesia Rupiah</Box>
-                <TextField
-                    sx={{ width: '30%' }}
-                    name='Enter Amount'
-                    label='IDR'
-                    InputProps={{
-                        // eslint-disable-next-line jsx-a11y/alt-text
-                        // startAdornment: <img src='/icons/navbar/account.png' />
-                        startAdornment: <Image src='/images/Rp.png' alt='picture' width={20} height={20} />
-                    }}
-                />
-            </Stack>
-            <Stack direction='row' spacing={5} sx={{ mt: 50 }}>
-                <ButtonBase sx={{ background: '#A54CE5', borderRadius: '4px', width: '150px', height: '60px' }}>
-                    <Typography sx={{ color: '#ffffff' }}>Submit</Typography>
-                </ButtonBase>
-                <ButtonBase
-                    sx={{ background: '#ffffff', border: '1px solid #A54CE5', borderRadius: '4px', width: '150px', height: '60px' }}
-                >
-                    <Typography sx={{ color: '#A54CE5' }}>Cancel</Typography>
-                </ButtonBase>
-            </Stack>
-        </Box>
+        <form onSubmit={form.handleSubmit(handleSubmit)}>
+            <Box sx={{ width: '100%' }}>
+                <Stack direction='row' spacing={30}>
+                    <Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center' }}>
+                        <Typography component='h3' fontSize='16px' fontWeight={600}>
+                            Effective
+                        </Typography>
+                        <FieldRequired />
+                    </Box>
+                    <Box sx={{ width: '30%' }}>
+                        <InputDate label='Effective Date' type='date' form={form} name={effectiveName} rules={{ required: true }} />
+                    </Box>
+                    {/* <TextField
+                        id='effective'
+                        sx={{ width: '30%' }}
+                        name='someDate'
+                        label='Effective Date'
+                        InputLabelProps={{ shrink: true, required: true }}
+                        type='date'
+                        defaultValue={values.someDate}
+                    /> */}
+                </Stack>
+                <Stack direction='row' spacing={30} sx={{ mt: 4 }}>
+                    <Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center' }}>
+                        <Typography component='h3' fontSize='16px' fontWeight={600}>
+                            Coins
+                        </Typography>
+                        <FieldRequired />
+                    </Box>
+                    <Box sx={{ width: '30%' }}>
+                        <Input
+                            type='number'
+                            rules={{ required: true }}
+                            form={form}
+                            name={coinsName}
+                            label='Amount'
+                            placeholder='Enter Amount'
+                            startAdornment={
+                                <img src='/images/coin.png' alt='coins' width={20} height={20} style={{ marginRight: '9px' }} />
+                            }
+                        />
+                    </Box>
+                    {/* <TextField
+                        id='coins'
+                        sx={{ width: '30%' }}
+                        name='Enter Amount'
+                        label='Enter Amount'
+                        InputProps={{
+                            // eslint-disable-next-line jsx-a11y/alt-text
+                            // startAdornment: <img src='/icons/navbar/account.png' />
+                            startAdornment: <Image src='/images/coin.png' alt='picture' width={20} height={20} />
+                        }}
+                    /> */}
+                </Stack>
+                <Stack direction='row' spacing={24} sx={{ mt: 4 }}>
+                    <Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center' }}>
+                        <Typography component='h3' fontSize='16px' fontWeight={600}>
+                            Indonesia Rupiah
+                        </Typography>
+                        <FieldRequired />
+                    </Box>
+                    <Box sx={{ width: '30%' }}>
+                        <Input
+                            type='number'
+                            rules={{ required: true }}
+                            form={form}
+                            name={idrName}
+                            label='IDR'
+                            placeholder='Enter Amount'
+                            startAdornment={
+                                <Typography component='span' fontSize='16px' fontWeight={900} sx={{ color: '#A54CE5', mr: '7px' }}>
+                                    RP
+                                </Typography>
+                            }
+                        />
+                    </Box>
+                    {/* <TextField
+                        id='idr'
+                        sx={{ width: '30%' }}
+                        name='Enter Amount'
+                        label='IDR'
+                        InputProps={{
+                            // eslint-disable-next-line jsx-a11y/alt-text
+                            // startAdornment: <img src='/icons/navbar/account.png' />
+                            startAdornment: <Image src='/images/Rp.png' alt='picture' width={20} height={20} />
+                        }}
+                    /> */}
+                </Stack>
+                <Stack direction='row' spacing={5} sx={{ mt: 50 }}>
+                    {loadingSubmit ? (
+                        <CircularProgress sx={{ ml: 5, color: '#A54CE5' }} />
+                    ) : (
+                        <>
+                            <ButtonBase
+                                disabled={loadingSubmit}
+                                type='submit'
+                                sx={{ background: '#A54CE5', borderRadius: '4px', width: '150px', height: '60px' }}
+                            >
+                                <Typography sx={{ color: '#ffffff' }}>Submit</Typography>
+                            </ButtonBase>
+                            <ButtonBase
+                                disabled={loadingSubmit}
+                                sx={{
+                                    background: '#ffffff',
+                                    border: '1px solid #A54CE5',
+                                    borderRadius: '4px',
+                                    width: '150px',
+                                    height: '60px'
+                                }}
+                            >
+                                <Typography sx={{ color: '#A54CE5' }}>Cancel</Typography>
+                            </ButtonBase>
+                        </>
+                    )}
+                </Stack>
+            </Box>
+        </form>
     );
 };
 
