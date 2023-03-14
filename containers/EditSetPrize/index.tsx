a1caf21badd6f89d2386f73e6b452e85d47026b6
ebennaga <freddy.eben@gmail.com>
2023-03-14 10:37:10 +0700

    fix props in edit set prize in client tournament

--- containers/EditSetPrize/index.tsx
+++ containers/EditSetPrize/index.tsx
@@ -35,7 +35,7 @@ const categoryList = [
 interface AddPrizeProps {
     statusEdit?: boolean;
 }
-const EditSetPrizeContainer: React.FC<AddPrizeProps> = ({ statusEdit = false }) => {
+const EditSetPrizeContainer: React.FC<AddPrizeProps> = ({ statusEdit = true }) => {
     const rules = { required: true };
     const { fetchAPI } = useAPICaller();
     const [isLoading, setIsLoading] = React.useState<boolean>(false);
@@ -154,7 +154,7 @@ const EditSetPrizeContainer: React.FC<AddPrizeProps> = ({ statusEdit = false }) 
 
     return (
         <Box component='section'>
-            <TitleCard title='Edit Set Prize' subtitle='Addtional description if required' isSearchExist={false} />
+            <TitleCard title='Edit Set Prize ' subtitle='Addtional description if required' isSearchExist={false} />
             <form onSubmit={form.handleSubmit(statusEdit ? handlePUTSubmit : handlePOSTSubmit)}>
                 <Box sx={{ my: 3, mx: 2, width: '40%' }}>
                     <Box sx={{ my: '30px' }}>
